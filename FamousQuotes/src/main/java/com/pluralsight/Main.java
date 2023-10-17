6d7d96db4a086b4784f72e298eddddf0c054aff3
Preen Shrestha <Shresthapreen@gmail.com>
2023-10-17 14:28:50 -0400

    First commit

---
+++ FamousQuotes/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String [] s = {"God blesses those who hunger and thirst for justice, for they will be satisfied."};
+
+    }
+}
