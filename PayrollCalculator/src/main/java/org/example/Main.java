6d86c83a0d872bd14ebdaa0a2d3b9e9e30bc1b90
Preen Shrestha <Shresthapreen@gmail.com>
2023-10-19 14:56:05 -0400

    Modified Project

--- PayrollCalculator/src/main/java/org/example/Main.java
+++ PayrollCalculator/src/main/java/org/example/Main.java
@@ -54,7 +54,7 @@ public class Main {
            }
            else{
                String s = String.format("""
-   {"id":" %d","name": "%s", "Gross-Pay": %.2f"},
+   {"id":" %d","name": "%s", "Gross-Pay": "%.2f"},
    """, e1.getEmployeeId(), e1.getName(), e1.getGrossPay());
 writer.write(s);
            }
