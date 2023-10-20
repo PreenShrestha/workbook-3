390519029f3b22331dbf0c65b9406a3d5fdb4937
Preen Shrestha <Shresthapreen@gmail.com>
2023-10-20 13:48:07 -0400

    Project with Time

---
+++ FormatDates/src/main/java/org/example/Main.java
@@ -0,0 +1,32 @@
+package org.example;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        // Get the current date and time in GMT time zone
+        LocalDateTime currentTime = LocalDateTime.now(TimeZone.getTimeZone("GMT").toZoneId());
+
+        // Create a date format pattern "MM/dd/yyyy"
+        DateTimeFormatter formattingDate = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        String fullDate = currentTime.format(formattingDate); //store it in fullDate variable and print out
+
+        DateTimeFormatter updatedFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String newFormattedDate = currentTime.format(updatedFormat);
+
+        DateTimeFormatter monthInWords = DateTimeFormatter.ofPattern("MMM dd, yyyy");
+        String monInWords = currentTime.format(monthInWords);
+
+        // Create a date format pattern with day of the week, month in words, and time
+        DateTimeFormatter dayInWeek = DateTimeFormatter.ofPattern("EEEE, MMM dd, yyyy hh:mm");
+        String dayInWeekWithMonInWords = currentTime.format(dayInWeek); //"Friday, Oct 20, 2023 05:40" - ex
+
+        // Print the formatted date and time strings
+        System.out.println(fullDate);
+        System.out.println(newFormattedDate);
+        System.out.println(monInWords);
+        System.out.println(dayInWeekWithMonInWords);
+    }
+}
